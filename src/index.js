88eec472553a360cb4f31ffe1d977808ddea2b53
DragonSlayer <rjkee13@gmail.com>
2025-11-07 22:40:08 -0500

    Bot is Up and Running!

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import { Application, Client, Events, GatewayIntentBits } from "discord.js";
+
+const TOKEN = process.env.TOKEN;
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.once(Events.ClientReady, (readyClient) => {
+    console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+});
+
+client.login(TOKEN);
+
