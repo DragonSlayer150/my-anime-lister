8aa5d7626bda866a912364229b538deda537f672
DragonSlayer <rjkee13@gmail.com>
2025-11-08 02:53:50 -0500

    Added the malbot command and slash command handling

---
+++ src/commands/utility/malbot.js
@@ -0,0 +1,34 @@
+const { ChannelType, SlashCommandBuilder } = require("discord.js");
+const { Keyv } = require("keyv");
+const { KeyvSqlite } = require("@keyv/sqlite");
+const keyv = new Keyv(new KeyvSqlite("sqlite://users.sqlite"));
+keyv.on('error', (err) => console.error('Keyv connection error:', err));
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("malbot")
+        .setDescription("Adds a new account to share watch/read history with")
+        .addSubcommand((subcommand) => 
+            subcommand
+                .setName("adduser")
+                .setDescription("Adds a new user to the listing")
+                .addStringOption((option) => 
+                    option
+                        .setName("username")
+                        .setDescription("Your MAL Username")
+                        .setRequired(true)))
+        .addSubcommand((subcommand) => 
+            subcommand
+                .setName("setchannel")
+                .setDescription("Sets the channel where the listing will show")
+                .addChannelOption((option) => 
+                    option
+                        .addChannelTypes(ChannelType.GuildText)
+                        .setName("channel")
+                        .setDescription("The channel that the listing will be sent in"))),
+                       
+                    
+    async execute(interaction) {
+        interaction.reply("The interaction of all time");
+    },
+};
--- src/index.js
+++ src/index.js
@@ -1,14 +1,69 @@
-import { Application, Client, Events, GatewayIntentBits } from "discord.js";
+const { Client, Collection, Events, GatewayIntentBits, MessageFlags } = require("discord.js");
+const fs = require('node:fs')
+const path = require('node:path');
 
+//Import Token from .env and create the Client
 const TOKEN = process.env.TOKEN;
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-client.once(Events.ClientReady, (readyClient) => {
-    console.log(`Ready! Logged in as ${readyClient.user.tag}`);
-    const channel = readyClient.channels.cache.get('1136134897837277204');
-    channel.send("Testing 123")
+
+//Dynamically stores all of the commands into client.commands
+client.commands = new Collection();
+
+const foldersPath = path.join(__dirname, 'commands');
+const commandFolders = fs.readdirSync(foldersPath);
+
+for (const folder of commandFolders) {
+	const commandsPath = path.join(foldersPath, folder);
+	const commandFiles = fs.readdirSync(commandsPath).filter((file) => file.endsWith('.js'));
+	for (const file of commandFiles) {
+		const filePath = path.join(commandsPath, file);
+		const command = require(filePath);
+		// Set a new item in the Collection with the key as the command name and the value as the exported module
+		if ('data' in command && 'execute' in command) {
+			client.commands.set(command.data.name, command);
+		} else {
+			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+		}
+	}
+}
+
+//preforms the commands execute() function, or replies an error
+client.on(Events.InteractionCreate, async (interaction) => {
+    if (!interaction.isChatInputCommand()) return;
+    const command = interaction.client.commands.get(interaction.commandName);
+
+    if (!command) {
+        console.error(`No command matching ${interaction.commandName} was found`);
+        return;
+    }
+    
+    try {
+        await command.execute(interaction);
+    } catch (error) {
+        console.error(error);
+        if (interaction.replied || interaction.deferred) {
+            await interaction.followUp({
+                content: 'There was an error while executing this command!',
+                flags: MessageFlags.Ephemeral
+            });
+        } else {
+            await interaction.reply({
+                content: 'There was an error while executing this command!',
+                flags: MessageFlags.Ephemeral,
+            });
+        }
+    }
+
 
 });
 
-client.login(TOKEN);
 
+
+//One time event listener for when the Client is ready, then sends a message to the console and to #bot-test
+client.once(Events.ClientReady, (readyClient) => {
+    console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+});
+
+//Login the bot
+client.login(TOKEN);
