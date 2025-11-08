70e65cd5bb4fd8461e2be601c72b893560725d4f
DragonSlayer <rjkee13@gmail.com>
2025-11-07 23:03:51 -0500

    Sends message to #bot-test upon start

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 client.once(Events.ClientReady, (readyClient) => {
     console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+    const channel = readyClient.channels.cache.get('1136134897837277204');
+    channel.send("Testing 123")
+
 });
 
 client.login(TOKEN);
